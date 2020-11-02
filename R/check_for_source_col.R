387d9d34eb49a370025c65eafcba5684fb23c44b
KaiAragaki <adamaragaki@gmail.com>
2020-11-02 12:53:53 -0500

    if and elses everywhere

--- R/check_for_source_col.R
+++ R/check_for_source_col.R
@@ -11,12 +11,21 @@ check_for_source_col <- function (url, token){
                                           url = url,
                                           http_verb = "GET") %>%
                 dplyr::as_tibble() %>%
-                tidyr::unnest_wider(.data$value) %>%
-                dplyr::filter(name == "source_code",
-                              read_only == F)
-        if (nrow(res) >= 1){
-                T
+                tidyr::unnest_wider(.data$value)
+        if ("source_code" %in% colnames(res)) {
+                if ("read_only" %in% colnames(res)){
+                        res <- dplyr::filter(name == "source_code",
+                                             read_only == F)
+                        if (nrow(res) >= 1) {
+                                res <- T
+                        } else {
+                                res <- F
+                        }
+                } else {
+                        res <- T
+                }
         } else {
-                F
+                res <- F
         }
+        res
 }
